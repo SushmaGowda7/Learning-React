ebcc0b5d75ac224f7a8b002dba3faafbca298313
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2022-09-18 20:27:16 +0530

    styling the Chartbars complete app

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpensesFilter from "./ExpensesFilter";
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
 
@@ -22,6 +23,7 @@ const Expenses = (props) => {
         selected ={filteredYear} 
         onChangeFilter={filterChangeHandler} 
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses}/>
     </Card>
     </div>
