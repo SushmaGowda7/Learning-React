9fcf8b61018122fa17e0fc5ed7097b7a35b8f9b0
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2022-09-18 15:09:19 +0530

    values are set dynamically

--- src/App.js
+++ src/App.js
@@ -1,50 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from "./components/NewExpense/NewExpense";
 import './App.css';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Bike Service',
+    amount: 2000,
+    location: 'Kolar',
+    date: new Date(2022, 7, 17)
+  },
+  {
+    id: 'e2',
+    title: 'New Car',
+    amount: 500000,
+    location: 'Bangalore',
+    date: new Date(2021, 8, 27)
+  },
+  {
+    id: 'e3',
+    title: 'Books',
+    amount: 500,
+    location: 'Kolar',
+    date: new Date(2022, 1, 18)
+  },
+  {
+    id: 'e4',
+    title: 'Movie',
+    amount: 300,
+    location: 'Malur',
+    date: new Date(2022, 1, 1)
+  },
+  {
+    id: 'e5',
+    title: 'Outing',
+    amount: 3000,
+    location: 'Ooty',
+    date: new Date(2022, 0, 1)
+  }
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Bike Service',
-      amount: 2000,
-      location: 'Kolar',
-      date: new Date(2022, 7, 17)
-    },
-    {
-      id: 'e2',
-      title: 'New Car',
-      amount: 500000,
-      location: 'Bangalore',
-      date: new Date(2021, 8, 27)
-    },
-    {
-      id: 'e3',
-      title: 'Books',
-      amount: 500,
-      location: 'Kolar',
-      date: new Date(2022, 1, 18)
-    },
-    {
-      id: 'e4',
-      title: 'Movie',
-      amount: 300,
-      location: 'Malur',
-      date: new Date(2022, 1, 1)
-    },
-    {
-      id: 'e5',
-      title: 'Outing',
-      amount: 3000,
-      location: 'Ooty',
-      date: new Date(2022, 0, 1)
-    }
-  ]
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log(expense);
-  }
+    setExpenses((prevExpenses) => {
+     return [expense, ...prevExpenses]
+  });
+};
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler}/>
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,33 +2,15 @@ import React, { useState } from "react";
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
-//import ExpenseDetails from './ExpenseDetails';
 
 const ExpenseItem = (props) => {
-    const [amount, setAmount] = useState( props.amount );
-    const changeHandler = () => {
-        setAmount('100');
-        console.log(amount);
-    }
-   const [title, setTitle] = useState( props.title );
-    const clickHandler = () => {
-        setTitle('updated');
-        console.log(title);
-    };
-
     return (
     <Card className='expense-item'>
        <ExpenseDate date={props.date}/>
-       {/* <ExpenseDetails amount={props.amount} /> */}
             <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__location'>
-                    <h3>At {props.location}</h3>
-                <div className='expense-item__price'>${amount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
-        </div>
-        <button onClick={changeHandler}>Change Expense</button>
-        <button onClick={clickHandler}>Edit Title</button>
     </Card>
     );
 }
